434bc7d9c3a441c025cabecd24a986a8b1b0a836
Bertalan Körmendy <korber314@gmail.com>
2017-09-03 12:17:59 +0200

    added signup form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import MenuBar from './MenuBar.js';
 import NewsFeed from './NewsFeed.js';
+import ReactSignupLoginComponent from 'react-signup-login-component';
 
 import './qpaweb.css';
 
@@ -12,6 +13,30 @@ const Info = () => (<p>Info</p>)
 const Achievementek = () => (<p>Achievementek</p>)
 const Riddles = () => (<p>Riddles</p>)
 const Swarm = () => (<p>Swarm</p>)
+const LoginPage = (props) => {
+    const signupWasClickedCallback = (data) => {
+      console.log(data);
+      alert('Signup callback, see log on the console to see the data.');
+    };
+    const loginWasClickedCallback = (data) => {
+      console.log(data);
+      alert('Login callback, see log on the console to see the data.');
+    };
+    const recoverPasswordWasClickedCallback = (data) => {
+      console.log(data);
+      alert('Recover password callback, see log on the console to see the data.');
+    };
+    return (
+        <div>
+            <ReactSignupLoginComponent
+                title="My awesome company"
+                handleSignup={signupWasClickedCallback}
+                handleLogin={loginWasClickedCallback}
+                handleRecoverPassword={recoverPasswordWasClickedCallback}
+            />
+        </div>
+    );
+};
 
 const App = () => (
   <Router>
@@ -25,6 +50,7 @@ const App = () => (
 
       <Route exact path="/" component={NewsFeed} />
       <Route path="/news" component={NewsFeed} />
+      <Route path="/login" component={LoginPage} />
       <Route path="/csapatok" component={Csapatok} />
       <Route path="/meres" component={Meres} />
       <Route path="/info" component={Info} />
