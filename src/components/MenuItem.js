6ff835d612db508c343c4b05585e4ce71116bee4
Adam Weber <Adam.Weber@POSSIBLE.com>
2017-09-08 11:20:20 +0200

    Implemented clickevents of menuitems and menuopener in mobile view

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom';
 import '../qpaweb.css'
 
 class MenuItem extends Component {
+  handleClick(e) {
+    console.log("ASDASD");
+    document.getElementById('menu').classList.remove('opened');
+  }
+
   render() {
     return(
       <Link to={this.props.to}>
-      <div className="menu__element-container">
+      <div className="menu__element-container" onClick={this.handleClick}>
         <div id="menu__element" className="menu__element">
           <img src={this.props.src} alt="background_image" />
           <a>{this.props.title}</a>
--- src/components/MenuOpener.js
+++ src/components/MenuOpener.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import '../qpaweb.css'
 
 class MenuOpener extends Component {
+  handleClick(e) {
+    document.getElementById('menu').classList.add('opened');
+  }
+
   render() {
     return(
-      <div id="menu__opener" className="menu__opener">MENU</div>
+      <div id="menu__opener" className="menu__opener" onClick={this.handleClick}>MENU</div>
     );
   }
 }
